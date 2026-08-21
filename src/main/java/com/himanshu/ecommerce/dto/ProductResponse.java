394d7e67f59bf2e36dee5a1d8086096b75f7d674
himanshu <himanshu.anand.5300@gmail.com>
2026-08-22 04:15:54 +0530

    updated product dtos

--- src/main/java/com/himanshu/ecommerce/dto/ProductResponse.java
+++ src/main/java/com/himanshu/ecommerce/dto/ProductResponse.java
@@ -1,4 +1,59 @@
 package com.himanshu.ecommerce.dto;
 
+import java.math.BigDecimal;
+
 public class ProductResponse {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private Integer stockQuantity;
+
+    public ProductResponse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(Integer stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
 }
