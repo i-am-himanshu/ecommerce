93a40d87020026f6bd410f34087e9eaf8fd2f247
himanshu <himanshu.anand.5300@gmail.com>
2026-08-27 03:25:41 +0530

    update Order service

--- src/main/java/com/himanshu/ecommerce/service/OrderService.java
+++ src/main/java/com/himanshu/ecommerce/service/OrderService.java
@@ -1,5 +1,10 @@
 package com.himanshu.ecommerce.service;
 
+import com.himanshu.ecommerce.dto.OrderRequest;
+import com.himanshu.ecommerce.dto.OrderResponse;
+import com.himanshu.ecommerce.model.Customer;
+import com.himanshu.ecommerce.model.Order;
+import com.himanshu.ecommerce.repository.CustomerRepository;
 import com.himanshu.ecommerce.repository.OrderRepository;
 import org.springframework.stereotype.Service;
 
@@ -7,10 +12,36 @@ import org.springframework.stereotype.Service;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final CustomerRepository customerRepository;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, CustomerRepository customerRepository) {
         this.orderRepository = orderRepository;
+        this.customerRepository = customerRepository;
     }
 
+    private OrderResponse mapToOrderResponse(Order order) {
+        OrderResponse orderResponse = new OrderResponse();
 
+        orderResponse.setCustomerId(order.getCustomer().getId());
+        orderResponse.setId(order.getId());
+        orderResponse.setOrderDate(order.getOrderDate());
+        orderResponse.setOrderStatus(order.getOrderStatus());
+
+        return orderResponse;
+    }
+
+    public OrderResponse createOrder(OrderRequest orderRequest) {
+        Customer customer = customerRepository.findById(orderRequest.getCustomerId())
+                .orElseThrow(() -> new RuntimeException("Customer not found."));
+
+        Order order = new Order();
+
+        order.setOrderDate(orderRequest.getOrderDate());
+        order.setOrderStatus(orderRequest.getOrderStatus());
+        order.setCustomer(customer);
+
+        Order savedOrder = orderRepository.save(order);
+
+        return mapToOrderResponse(savedOrder);
+    }
 }
