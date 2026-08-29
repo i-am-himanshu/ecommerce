dba9fe5c9baa2daf738bfd2aa7f6c1bbfca5e7df
himanshu <himanshu.anand.5300@gmail.com>
2026-08-30 03:05:05 +0530

    update customer service to get orders

--- src/main/java/com/himanshu/ecommerce/service/CustomerService.java
+++ src/main/java/com/himanshu/ecommerce/service/CustomerService.java
@@ -2,9 +2,13 @@ package com.himanshu.ecommerce.service;
 
 import com.himanshu.ecommerce.dto.CustomerRequest;
 import com.himanshu.ecommerce.dto.CustomerResponse;
+import com.himanshu.ecommerce.dto.OrderSummaryResponse;
 import com.himanshu.ecommerce.model.Customer;
+import com.himanshu.ecommerce.model.Order;
 import com.himanshu.ecommerce.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
@@ -36,5 +40,33 @@ public class CustomerService {
         Customer savedCustomer = customerRepository.save(customer);
 
         return mapToCustomerResponse(savedCustomer);
+    }
+
+    public CustomerResponse getCustomer(Long id) {
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Customer not found."));
+
+        CustomerResponse response = new CustomerResponse();
+
+        response.setId(customer.getId());
+        response.setEmail(customer.getEmail());
+        response.setName(customer.getName());
+
+        List<OrderSummaryResponse> orders = customer.getOrders()
+                .stream()
+                .map(order -> {
+                    OrderSummaryResponse orderSummaryResponse = new OrderSummaryResponse();
+
+                    orderSummaryResponse.setId(order.getId());
+                    orderSummaryResponse.setOrderDate(order.getOrderDate());
+                    orderSummaryResponse.setOrderStatus(order.getOrderStatus());
+
+                    return  orderSummaryResponse;
+                })
+                .toList();
+
+        response.setOrders(orders);
+
+        return response;
     }
 }
