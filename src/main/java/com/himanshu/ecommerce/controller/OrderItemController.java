81bdcb44eb44882af73f4fbae630887931137938
himanshu <himanshu.anand.5300@gmail.com>
2026-09-01 20:55:22 +0530

    add order item controller

---
+++ src/main/java/com/himanshu/ecommerce/controller/OrderItemController.java
@@ -0,0 +1,25 @@
+package com.himanshu.ecommerce.controller;
+
+import com.himanshu.ecommerce.dto.OrderItemRequest;
+import com.himanshu.ecommerce.model.OrderItem;
+import com.himanshu.ecommerce.service.OrderItemService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order-items")
+public class OrderItemController {
+
+    private final OrderItemService orderItemService;
+
+    public OrderItemController(OrderItemService orderItemService) {
+        this.orderItemService = orderItemService;
+    }
+
+    @PostMapping
+    public OrderItem createOrderItem(@RequestBody OrderItemRequest orderItemRequest) {
+        return orderItemService.createOrderItem(orderItemRequest);
+    }
+}
