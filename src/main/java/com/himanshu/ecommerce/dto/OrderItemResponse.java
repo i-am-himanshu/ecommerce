839da11b377b119c5adc68f485fe44edb24b42e2
himanshu <himanshu.anand.5300@gmail.com>
2026-09-01 20:58:50 +0530

    add order item response dto

---
+++ src/main/java/com/himanshu/ecommerce/dto/OrderItemResponse.java
@@ -0,0 +1,59 @@
+package com.himanshu.ecommerce.dto;
+
+import java.math.BigDecimal;
+
+public class OrderItemResponse {
+
+    private Long id;
+
+    private Integer quantity;
+
+    private BigDecimal price;
+
+    private Long orderId;
+
+    private Long productId;
+
+    public OrderItemResponse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+}
